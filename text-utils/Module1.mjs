9a85fb8742b3e816892ea4af39eb140191271fa0
shashwat051102 <137853743+shashwat051102@users.noreply.github.com>
2024-06-26 21:05:03 +0530

    Delete text-utils directory

--- text-utils/Module1.mjs
+++
@@ -1,9 +0,0 @@
-import ui, {a,c,b} from './Module2.mjs'
-
-console.log(ui);
-
-console.log(a);
-
-console.log(b);
-
-console.log(c);
--- text-utils/Module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = "Harry";
-const b = "Rohan";
-const c = "Aakash";
-const d = "Priyankan";
-
-export default c;
-export {a};
-export {b};
-export {c};
